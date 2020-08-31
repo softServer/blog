cdb58770517c15b43591f42ec758b3b8f2536178
zhanggaojie <zhanggaojie@yy.com>
2020-08-31 19:01:45 +0800

    first commit

---
+++ src/components/core/Cta.vue
@@ -0,0 +1,35 @@
+<!-- cat是call to action 的缩写，中午通常称之为“行为召唤”。
+    通过设计引导甚至诱导用户执行的特定操作，
+    例如通过点击留下联系方式，进行购买等都是行为召唤。 
+-->
+<template>
+  <v-btn
+    :style="styles"
+    color="secondary"
+    fab
+    fixed
+    href="https://store.vuetifyjs.com/product/blog-theme-free"
+    rel="noopener"
+    right
+    target="_blank"
+    top
+  >
+    <v-icon>
+      mdi-vuetify
+    </v-icon>
+  </v-btn>
+</template>
+
+<script>
+  export default {
+    name: 'CoreCta',
+
+    computed: {
+      styles () {
+        return {
+          marginTop: `${this.$vuetify.application.top}px`,
+        }
+      },
+    },
+  }
+</script>
